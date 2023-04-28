bf4a913a5be005d7acd78c2bfd9a5203d7b0c59d
Matt Walker <matt.walker@genesis.global>
2023-04-28 13:47:45 +0100

    setup cem analyze

---
+++ custom-elements-manifest.config.js
@@ -0,0 +1,11 @@
+// export default {
+module.exports = {
+  /** Globs to analyze */
+  globs: ['src/**/*.ts'],
+  /** Directory to output CEM to */
+  outdir: './dist',
+  /** Run in dev mode, provides extra logging */
+  dev: false,
+  /** Enable special handling for FastElement */
+  fast: true,
+};
